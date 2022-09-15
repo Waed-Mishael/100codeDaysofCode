e50003e6829abba310389324333068741e6bbd57
Waed-Mishael <113536443+Waed-Mishael@users.noreply.github.com>
2022-09-15 10:18:48 +0300

    my solution

--- Working with datetimes.py
+++ Working with datetimes.py
@@ -1,3 +1,4 @@
+import datetime
 from datetime import date, timedelta
 
 start_100days = date(2017, 3, 30)
@@ -7,9 +8,11 @@ pycon_date = date(2018, 5, 8)
 
 def get_hundred_days_end_date():
     """Return a string of yyyy-mm-dd"""
-    pass
-
+    plus_100 = timedelta(100)
+    after_100dayes = start_100days + plus_100
+    after_100dayes_str = "{:%Y-%m-%d}"
+    return after_100dayes_str.format(after_100dayes)
 
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
-    pass
+    return (pycon_date - pybites_founded).days
