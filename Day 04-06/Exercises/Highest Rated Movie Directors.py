c48952691b89b225f31e8c8ff327c8d607d371be
Waed-Mishael <113536443+Waed-Mishael@users.noreply.github.com>
2022-09-20 23:10:36 +0300

    I finished the challenge and added 2 function that I think are cool!

--- Day 04-06/Exercises/Highest Rated Movie Directors.py
+++ Day 04-06/Exercises/Highest Rated Movie Directors.py
@@ -2,24 +2,57 @@
 average movie IMDB ratings. """
 
 import csv
-import datetime
+import collections
+
 
 path = "movie_metadata.csv"
 file = open(path, newline='', encoding='utf-8')
 reader = csv.reader(file)
 header = next(reader)
+his_movie = collections.namedtuple('his_movie', ['Title', 'Year', 'Score'])
 
-data = []
 
-for line in reader:
-    try:
-        director_name = line[1]  # Director name
-        movie_title = line[11]  # Movie title
-        title_year = datetime.datetime.strptime(line[23], '%Y')  # Title year
-        imdb_score = float(line[25])  # Imdb score
-    except ValueError:
-        continue
+def get_important_data(the_file):
+    data = []
 
-    data.append([director_name, movie_title, title_year, imdb_score])
+    directors = collections.defaultdict(list)
+    for line in the_file:
+        try:
+            director_name = line[1]  # Director name
+            movie_title = line[11].replace('\xa0', '')  # Movie title
+            title_year = int(line[23])  # Title year
+            imdb_score = float(line[25])  # Imdb score
+        except ValueError:
+            continue
+        director_movies = his_movie(movie_title, title_year, imdb_score)
+        directors[director_name].append(director_movies)
+    return directors
 
-print(data)
+
+directors_dict = get_important_data(reader)
+
+
+def most_famous_directors(top_i):
+    count = collections.Counter()
+    for director, his_movies in directors_dict.items():
+        count[director] += len(his_movies)
+    return count.most_common(top_i)
+
+
+def lst_all_movies_with_the_score(j):
+    lst = []
+    for director, s_movie in directors_dict.items():
+        if s_movie[0].Score == j:
+            lst.append((director, directors_dict[director][0].Title, directors_dict[director][0].Year))
+    return lst
+
+
+def top_movies(num):
+    sorted_lst = []
+    for i in range(100, -1, -1):
+        wanted_score = i/10
+        the_movie_lst = lst_all_movies_with_the_score(wanted_score)
+        if len(the_movie_lst) > 0:
+            for j in range(len(the_movie_lst)):
+                sorted_lst.append((the_movie_lst[j][1], the_movie_lst[j][2]))  # Give just the title and year
+    return sorted_lst[0:num]
